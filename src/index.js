0a922465947e1089ff8384bb4ea43e40e8bf5abd
bigpimplimp <levi.jcecil@gmail.com>
2025-08-12 15:46:07 -0400

    small update

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-// import './style.css';
+import './style.css';
 import { Gameboard } from './gameboard';
 import { Ship } from './ship';
 import { Player } from './player';
 console.log('Connected');
 
 
-export const carrier = new Ship(3, 'carrier')
-carrier.hit()
-carrier.hit()
-export const gb = new Gameboard();
-gb.placeShip([0, 1], carrier)
-// gb.recieveAttack([0, 1]);
+// export const carrier = new Ship(3, 'carrier')
+// carrier.hit()
+// carrier.hit()
+// export const gb = new Gameboard();
+// gb.placeShip([0, 1], carrier)
+// // gb.recieveAttack([0, 1]);
 
-document.addEventListener('DOMContentLoaded', () => {
+
+
+//probably better to use table instead of grid
+document.addEventListener('DOMContentLoaded', () => { 
   const playerGridContainer = document.getElementById('player-board');
   const enemyGridContainer = document.getElementById('enemy-board');
-
   const gridSize = 10;
 
   for (let i = 0; i < gridSize; i ++) {
@@ -24,7 +26,25 @@ document.addEventListener('DOMContentLoaded', () => {
       cell.classList.add('grid-cell');
       cell.innerHTML = 'x'
       playerGridContainer.appendChild(cell);
-      // enemyGridContainer.appendChild(cell);
+    }
+  }
+
+  for (let i = 0; i < gridSize; i ++) {
+    for (let j = 0; j < gridSize; j++) {
+      const cell = document.createElement('div');
+      cell.classList.add('grid-cell');
+      cell.innerHTML = 'x'
+    enemyGridContainer.appendChild(cell);
     }
   }
 })
+
+
+const player1 = new Player ();
+const npc = new Player ();
+
+player1.myBoard.placeShip([0, 7], 'carrier');
+
+const gridItem = document.querySelector('[style*="grid-column: 9"][style*="grid-row: 1"]');
+gridItem.innerHTML = 'adf'
+
